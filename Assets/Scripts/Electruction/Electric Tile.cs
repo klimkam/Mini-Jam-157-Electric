91e8836009809fcf18a6fcaecd98698f385b8371
Klimentiy Mandra <56902939+klimkam@users.noreply.github.com>
2024-04-28 21:27:57 -0400

    getting the player

--- Assets/Scripts/Electruction/Electric Tile.cs
+++ Assets/Scripts/Electruction/Electric Tile.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ElectricTile : MonoBehaviour
 {
-    [SerializeField]
-    PlayerController _player;
+    GameObject _player;
+    PlayerController _playerController;
     private float _lifeTime = 2f;
+    float _chargeTime = 0f;
 
     private void Start()
     {
+        _player = GameObject.Find("Player");
+
+        _playerController = _player.GetComponent<PlayerController>();
         StartCoroutine(DeleteTile());
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
-            _player.DeactivateAllLines();
-            _player.LockPlayerMovement();
+            _playerController.DeactivateAllLines();
+            _playerController.LockPlayerMovement();
         }
     }
 
